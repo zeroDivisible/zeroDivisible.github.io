1c163f358a8344d9443e20b515e2aeb54d46fa21
zerodivisible <zerodivisible@gmail.com>
2014-09-28 11:23:42 +0100

    some settings and stuff

--- pelicanconf.py
+++ pelicanconf.py
@@ -38,9 +38,16 @@ AUTHOR_SAVE_AS = 'author/{slug}/index.html'
 YEAR_ARCHIVE_SAVE_AS = 'blog/{date:%Y}/index.html'
 MONTH_ARCHIVE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/index.html'
 
+ARCHIVES_URL = 'archives/'
 ARCHIVES_SAVE_AS = 'archives/index.html'
+
+AUTHORS_URL = 'authors/'
 AUTHORS_SAVE_AS = 'authors/index.html'
+
+CATEGORIES_URL = 'categories/'
 CATEGORIES_SAVE_AS = 'categories/index.html'
+
+TAGS_URL = 'tags/'
 TAGS_SAVE_AS = 'tags/index.html'
 
 # Social widget
@@ -57,6 +64,7 @@ CUSTOM_CSS = 'static/custom.css'
 # visual related settings
 THEME = './pelican-bootstrap/'
 BOOTSTRAP_THEME = 'myunited'
+#BOOTSTRAP_THEME = 'paper'
 PYGMENTS_STYLE = 'colorful'
 BOOTSTRAP_NAVBAR_INVERSE = True
 HIDE_SIDEBAR = True
