3a52f28077e1c97e7a84704e0672dc7cb1c1c2d1
zerodivisible <zerodivisible@gmail.com>
2014-09-28 21:48:29 +0100

    conf changes

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,7 +6,6 @@ AUTHOR = u'Michal Stolarczyk'
 SITENAME = u'a blog'
 SITEURL = ''
 #RELATIVE_URLS = True
-#DEFAULT_PAGINATION = 10
 TIMEZONE = 'Europe/Dublin'
 DEFAULT_LANG = u'en'
 
@@ -16,11 +15,13 @@ PATH = 'content'
 ARTICLE_URL = "blog/{date:%Y}/{date:%m}/{slug}/"
 ARTICLE_SAVE_AS = "blog/{date:%Y}/{date:%m}/{slug}/index.html"
 
-CATEGORY_URL = "category/{slug}/"
-CATEGORY_SAVE_AS = "category/{slug}/index.html"
+CATEGORY_URL = 'category/{slug}/'
+CATEGORY_SAVE_AS = 'category/{slug}/index.html'
 
-TAG_URL = "tag/{slug}/"
-TAG_SAVE_AS = "tag/{slug}/index.html"
+TAG_URL = 'tag/{slug}/'
+TAG_SAVE_AS = 'tag/{slug}/index.html'
+TAGS_URL= 'tags/'
+TAGS_SAVE_AS = 'tags/index.html'
 
 AUTHOR_URL = 'author/{slug}/'
 AUTHOR_SAVE_AS = 'author/{slug}/index.html'
@@ -32,7 +33,6 @@ MONTH_ARCHIVE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/index.html'
 ARCHIVES_SAVE_AS = 'archives/index.html'
 AUTHORS_SAVE_AS = ''
 CATEGORIES_SAVE_AS = 'categories/index.html'
-TAGS_SAVE_AS = 'tags/index.html'
 
 # Social widget
 SOCIAL = (('twitter', 'https://twitter.com/zeroDivisible'),
@@ -41,8 +41,8 @@ SOCIAL = (('twitter', 'https://twitter.com/zeroDivisible'),
 # custom css
 STATIC_PATHS = ['static', 'extra/custom.css']
 EXTRA_PATH_METADATA = {
-    'extra/custom.css': {'path': 'static/custom.css'},
-}
+        'extra/custom.css': {'path': 'static/custom.css'},
+        }
 CUSTOM_CSS = 'static/custom.css'
 
 # visual related settings
