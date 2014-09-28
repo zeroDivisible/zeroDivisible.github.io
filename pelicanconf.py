269d9281fd102a60d5d5675eed0e03a7d632b88f
zerodivisible <zerodivisible@gmail.com>
2014-09-28 21:23:28 +0100

    style fixes

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,23 +3,14 @@
 from __future__ import unicode_literals
 
 AUTHOR = u'Michal Stolarczyk'
-SITENAME = u'null log'
-SITEURL = 'http://zerodivisible.io'
+SITENAME = u'a blog'
+SITEURL = ''
 #RELATIVE_URLS = True
-GITHUB_USER = u'zeroDivisible'
-DEFAULT_PAGINATION = 10
+#DEFAULT_PAGINATION = 10
 TIMEZONE = 'Europe/Dublin'
 DEFAULT_LANG = u'en'
 
 PATH = 'content'
-
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-
-# Blogroll
-LINKS = ()
 
 #url settings
 ARTICLE_URL = "blog/{date:%Y}/{date:%m}/{slug}/"
@@ -50,20 +41,20 @@ SOCIAL = (('twitter', 'https://twitter.com/zeroDivisible'),
 # custom css
 STATIC_PATHS = ['static', 'extra/custom.css']
 EXTRA_PATH_METADATA = {
-    'extra/custom.css': {'path': 'static/custom.css'}
+    'extra/custom.css': {'path': 'static/custom.css'},
 }
 CUSTOM_CSS = 'static/custom.css'
 
 # visual related settings
 THEME = './pelican-bootstrap/'
 BOOTSTRAP_THEME = 'myunited'
-#BOOTSTRAP_THEME = 'paper'
 PYGMENTS_STYLE = 'colorful'
 BOOTSTRAP_NAVBAR_INVERSE = True
-HIDE_SIDEBAR = True
-
-
-PYGMENTS_MD_OPTIONS = {'linenos': 'table'}
+HIDE_SIDEBAR = False
+DISPLAY_BREADCRUMBS = False
+USE_PAGER = True
+DISPLAY_TAGS_INLINE = True
+DISPLAY_RECENT_POSTS_ON_SIDEBAR = True
 
 GOOGLE_ANALYTICS = 'UA-47086458-1'
 
